74f9c592d300bb4e3ac1ea70566200616daf88cc
PedroPK <pedropk@gmail.com>
2022-04-05 18:59:20 -0300

    More Tests implemented

--- venv/src/leilao/TestAuctionEvaluator.py
+++ venv/src/leilao/TestAuctionEvaluator.py
@@ -5,7 +5,7 @@ from dominio import Usuario, Lance, Leilao, AuctionEvaluator
 
 class TestAuctionEvaluator(TestCase):
 
-    def test_evaluate_smallerBidFirst(self):
+    def test_evaluate_twoBids_withSmallerBidFirst(self):
         gui     =   Usuario('Guilherme')
         yuri    =   Usuario('Yuri')
 
@@ -25,7 +25,7 @@ class TestAuctionEvaluator(TestCase):
         self.assertEqual(menorExpected, evaluator.smaller_bid)
         self.assertEqual(maiorExperado, evaluator.bigger_bid)
 
-    def test_evaluate_biggerBidFirst(self):
+    def test_evaluate_twoBids_withBiggerBidFirst(self):
         gui     =   Usuario('Guilherme')
         yuri    =   Usuario('Yuri')
 
@@ -41,6 +41,43 @@ class TestAuctionEvaluator(TestCase):
 
         menorExpected   =   100.0
         maiorExperado   =   150.0
+
+        self.assertEqual(menorExpected, evaluator.smaller_bid)
+        self.assertEqual(maiorExperado, evaluator.bigger_bid)
+
+    def test_evaluate_withOnlyOneBid(self):
+        gui     =   Usuario("Guilherme")
+
+        lance   =   Lance(gui, 150.0)
+
+        leilao  =   Leilao("Celular")
+        leilao.lances.append(lance)
+
+        evaluator   =   AuctionEvaluator()
+        evaluator.evaluate(leilao)
+
+        self.assertEqual(150.0,     evaluator.smaller_bid )
+        self.assertEqual(150.0,     evaluator.bigger_bid )
+
+    def test_evaluate_withThreeBids_inGrowingOrder(self):
+        gui     =   Usuario('Guilherme')
+        yuri    =   Usuario('Yuri')
+        vini    =   Usuario("Vinicius")
+
+        lance_yuri  =   Lance(yuri, 100.0)
+        lance_gui   =   Lance(gui, 150.0)
+        lance_vini  =   Lance(vini, 200.0)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance_yuri)
+        leilao.lances.append(lance_gui)
+        leilao.lances.append(lance_vini)
+
+        evaluator = AuctionEvaluator()
+        evaluator.evaluate(leilao)
+
+        menorExpected = 100.0
+        maiorExperado = 200.0
 
         self.assertEqual(menorExpected, evaluator.smaller_bid)
         self.assertEqual(maiorExperado, evaluator.bigger_bid)
